0caaac149c73c1f9c968d1352e13ec9233970a3b
aydie <aydieprodstudio@gmail.com>
2025-08-03 23:58:59 +0530

    Added urls for dashboard

---
+++ apps/dashboard/urls.py
@@ -0,0 +1,13 @@
+from django import path
+from .views import DashboardView
+
+# The app_name variable helps Django distinguish between URL names
+# From different apps
+
+app_name = 'dashboard'
+
+urlpatterns = [
+    # This maps the root URL of this app (/dashboard/) to our main view.
+    # The name 'dashboard' will be used in templates and redirects.
+    path('', DashboardView.as_view(), name='dashboard')
+]
